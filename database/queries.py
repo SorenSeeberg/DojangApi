dc8960e47a66e14d6ea84f3228d2cb4aee434708
Søren <soren.seeberg@gmail.com>
2019-04-04 20:24:00 +0200

    Working on queries

--- database/queries.py
+++ database/queries.py
@@ -3,7 +3,6 @@ from database.tables import *
 from uuid import uuid4
 from hashlib import sha3_256
 
-
 categories: List[Category] = list()
 knowledge_base: List[KnowledgePairs] = list()
 active_quizzes: List[ActiveQuiz] = list()
@@ -13,4 +12,19 @@ users: List[User] = list()
 
 
 def new_user(email: str, password: str) -> None:
-    users.append(User(id=str(uuid4()), email=email, password_hash=sha3_256(password)))
+    users.append(User(id=str(uuid4()), email=email, password_hash=sha3_256(password.encode()).hexdigest()))
+
+
+def new_quiz() -> None:
+    pass
+
+
+def new_question() -> None:
+    pass
+
+
+if __name__ == '__main__':
+    new_user('soren.seeberg@gmail.com', 'abc1234')
+    new_user('sorense@configit.com', 'ab134')
+
+[print(user) for user in users]
