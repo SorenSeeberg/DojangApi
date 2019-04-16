dcc5ea5b17507488c9a4bf99d9755a9279d0722e
Søren <soren.seeberg@gmail.com>
2019-04-16 17:33:28 +0200

    - working on database design

--- database/queries.py
+++ database/queries.py
@@ -5,7 +5,7 @@ from hashlib import sha3_256
 import random
 
 categories: List[Category] = list()
-knowledge_base: List[KnowledgePair] = list()
+knowledge_base: List[KnowledgeKeyValue] = list()
 active_quizzes: List[Quiz] = list()
 questions: List[Question] = list()
 answers: List[Answer] = list()
@@ -27,25 +27,25 @@ def setup_categories():
 
 def setup_knowledge_pairs():
     knowledge_base.extend([
-        KnowledgePair(id=1, lang_one='black', lang_two='sort', category_id=1),
-        KnowledgePair(id=2, lang_one='white', lang_two='hvid', category_id=1),
-        KnowledgePair(id=3, lang_one='red', lang_two='rød', category_id=1),
-        KnowledgePair(id=4, lang_one='green', lang_two='grøn', category_id=1),
-        KnowledgePair(id=5, lang_one='blue', lang_two='blå', category_id=1),
-        KnowledgePair(id=6, lang_one='yellow', lang_two='gul', category_id=1),
-        KnowledgePair(id=7, lang_one='house', lang_two='hus', category_id=2),
-        KnowledgePair(id=8, lang_one='sky scraper', lang_two='skyskraber', category_id=2),
-        KnowledgePair(id=9, lang_one='hut', lang_two='hytte', category_id=2),
-        KnowledgePair(id=10, lang_one='farm', lang_two='bondegård', category_id=2)
+        KnowledgeKeyValue(id=1, key='black', value='sort', category_id=1),
+        KnowledgeKeyValue(id=2, key='white', value='hvid', category_id=1),
+        KnowledgeKeyValue(id=3, key='red', value='rød', category_id=1),
+        KnowledgeKeyValue(id=4, key='green', value='grøn', category_id=1),
+        KnowledgeKeyValue(id=5, key='blue', value='blå', category_id=1),
+        KnowledgeKeyValue(id=6, key='yellow', value='gul', category_id=1),
+        KnowledgeKeyValue(id=7, key='house', value='hus', category_id=2),
+        KnowledgeKeyValue(id=8, key='sky scraper', value='skyskraber', category_id=2),
+        KnowledgeKeyValue(id=9, key='hut', value='hytte', category_id=2),
+        KnowledgeKeyValue(id=10, key='farm', value='bondegård', category_id=2)
     ])
 
 
-def pick_random_knowledge_pair_id(include_categories: List[int], used_ids: List[int]) -> KnowledgePair:
+def pick_random_knowledge_pair_id(include_categories: List[int], used_ids: List[int]) -> KnowledgeKeyValue:
     return random.choice([k for k in knowledge_base if k.category_id in include_categories and k.id not in used_ids])
 
 
-def pick_knowledge_pairs(pair_count: int, include_categories: List[int]) -> List[KnowledgePair]:
-    pairs: List[KnowledgePair] = list()
+def pick_knowledge_pairs(pair_count: int, include_categories: List[int]) -> List[KnowledgeKeyValue]:
+    pairs: List[KnowledgeKeyValue] = list()
 
     for x in range(pair_count):
         pairs.append(pick_random_knowledge_pair_id(
@@ -57,19 +57,20 @@ def pick_knowledge_pairs(pair_count: int, include_categories: List[int]) -> List
 
 
 def new_user(email: str, password: str) -> None:
-    users.append(User(id=str(uuid4()), email=email, password_hash=sha3_256(password.encode()).hexdigest()))
+    users.append(
+        User(id=len(users) + 1, email=email, password_hash=sha3_256(password.encode()).hexdigest(), confirmed=True))
 
 
 def new_question(quiz_id: str, included_category_ids: List[int], option_count: int) -> None:
-
     question_id: str = str(uuid4())
 
-    _pairs: List[KnowledgePair] = pick_knowledge_pairs(option_count, included_category_ids)
-    _options: List[Option] = [Option(id=str(uuid4()), question_id=question_id, option=option.lang_two) for index, option in enumerate(_pairs)]
+    _pairs: List[KnowledgeKeyValue] = pick_knowledge_pairs(option_count, included_category_ids)
+    _options: List[Option] = [Option(id=str(uuid4()), question_id=question_id, option=option.value) for index, option in
+                              enumerate(_pairs)]
 
     answer_index = random.randrange(option_count)
 
-    _answer_pair: KnowledgePair = _pairs[answer_index]
+    _answer_pair: KnowledgeKeyValue = _pairs[answer_index]
     _answer_option: Option = _options[answer_index]
 
     options.extend(_options)
@@ -79,7 +80,7 @@ def new_question(quiz_id: str, included_category_ids: List[int], option_count: i
         quiz_id=quiz_id,
         option_count=option_count,
         knowledge_pair_id=_answer_pair.id,
-        text=_answer_pair.lang_one,
+        text=_answer_pair.key,
         answer_id=_answer_option.id)
     )
 
@@ -87,7 +88,8 @@ def new_question(quiz_id: str, included_category_ids: List[int], option_count: i
 def new_quiz(question_count: int, option_count: int, included_category_ids: List[int]) -> None:
     quiz_id = str(uuid4())
 
-    [new_question(quiz_id=quiz_id, included_category_ids=included_category_ids, option_count=option_count) for x in range(question_count)]
+    [new_question(quiz_id=quiz_id, included_category_ids=included_category_ids, option_count=option_count) for x in
+     range(question_count)]
     quizzes.append(Quiz(
         id=quiz_id,
         question_count=question_count,
@@ -101,15 +103,16 @@ def new_quiz(question_count: int, option_count: int, included_category_ids: List
 if __name__ == '__main__':
     setup_categories()
     setup_knowledge_pairs()
-    # new_user('soren.seeberg@gmail.com', 'abc1234')
-    # new_user('sorense@configit.com', 'ab134')
-    #
-    # [print(user) for user in users]
+    new_user('soren.seeberg@gmail.com', 'abc1234')
+    new_user('sorense@configit.com', 'ab134')
 
     quiz_id = 'quiz666'
 
     # new_question(quiz_id=quiz_id, included_category_ids=[1], option_count=5)
     new_quiz(question_count=20, option_count=4, included_category_ids=[1])
-    print_table(quizzes)
-    print_table(questions)
-    print_table(options)
+    # print_table(quizzes)
+    # print_table(questions)
+    # print_table(options)
+    print_table(users)
+    print(len(users[0].password_hash))
+    print(len(quizzes[0].id))
--- database/tables.py
+++ database/tables.py
@@ -3,54 +3,68 @@ from dataclasses import dataclass
 
 @dataclass
 class Category:
-    id: int
+    id: int  # PK
     name: str
 
 
 @dataclass
-class KnowledgePair:
-    id: int
-    lang_one: str
-    lang_two: str
-    category_id: int
+class KnowledgeKeyValue:
+    id: int  # PK
+    key: str
+    value: str
+    category_id: int  # FK -> Category.id
 
 
 @dataclass
 class Option:
-    id: str
+    id: str  # PK
     option: str
-    question_id: str
+    question_id: str  # FK -> Question.id
 
 
 @dataclass
 class Question:
-    id: str
-    quiz_id: str
-    option_count: int
-    knowledge_pair_id: int
+    id: str  # PK
     text: str
-    answer_id: str
+    option_count: int
+    quiz_id: str  # FK -> Quiz.id
+    knowledge_pair_id: int  # FK -> KnowledgePair.id
+    answer_id: str  # FK -> Answer.id
 
 
 @dataclass
 class Quiz:
-    id: str
+    id: str  # PK
     question_count: int
     current_question: int
     title: str
     created: int
-    user_id: int
+    user_id: int  # FK -> User.id
 
 
 @dataclass
 class Answer:
-    id: str
-    knowledgeId: str
+    id: str  # PK
+    knowledge_key_value_id: str  # FK -> KnowledgeKeyValue.id
     correct: bool
 
 
 @dataclass
 class User:
-    id: str
-    email: str
+    id: int  # PK
+    email: str  # Unique
     password_hash: str
+    confirmed: bool
+
+
+@dataclass
+class Belt:
+    id: int
+    name: str
+
+
+@dataclass
+class QuizCategory:
+    id: int
+    quiz_id: str
+    category_id: int
