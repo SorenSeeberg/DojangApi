8fdfada518d7230ce980ac4b39e01a945bf1a387
Søren <soren.seeberg@gmail.com>
2019-04-17 21:46:47 +0200

    - converting table defs to classes

--- database/tables.py
+++ database/tables.py
@@ -1,112 +1,127 @@
-
 from sqlalchemy import create_engine, MetaData, Table, Column
-from sqlalchemy import INT, SMALLINT, BOOLEAN, VARCHAR, CHAR, NVARCHAR
+from sqlalchemy import INT, SMALLINT, BOOLEAN, VARCHAR, CHAR, NVARCHAR, Integer
 from sqlalchemy import ForeignKey
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 from config import *
 
-engine = create_engine(DB_CONNECTION_STRING, echo=True)
+Base = declarative_base()
 meta = MetaData()
 
 # Static Data Tables
 
-INFO = Table(
-    'Info', meta,
-    Column('id', INT, primary_key=True),
-    Column('key', NVARCHAR(TEXT_LENGTH)),
-    Column('value', NVARCHAR(TEXT_LENGTH)),
-    Column('categoryId', INT, ForeignKey('Category.id')),
-    Column('beltId', SMALLINT, ForeignKey('Belt.id'))
-)
+# INFO = Table(
+#     'Info', meta,
+#     Column('id', INT, primary_key=True),
+#     Column('key', NVARCHAR(TEXT_LENGTH)),
+#     Column('value', NVARCHAR(TEXT_LENGTH)),
+#     Column('categoryId', INT, ForeignKey('Category.id')),
+#     Column('beltId', SMALLINT, ForeignKey('Belt.id'))
+# )
 
-CATEGORY = Table(
-    'Category', meta,
-    Column('id', SMALLINT, primary_key=True),
-    Column('name', VARCHAR(LABEL_LENGTH))
-)
 
-BELT = Table(
-    'Belt', meta,
-    Column('id', SMALLINT, primary_key=True),
-    Column('name', VARCHAR(LABEL_LENGTH))
-)
+class Category(Base):
+    __tablename__ = "Category"
+
+    id = Column(INT, primary_key=True)
+    name = Column(VARCHAR(LABEL_LENGTH))
+
+
+class Belt(Base):
+    __tablename__ = "Belt"
+
+    id = Column(INT, primary_key=True)
+    name = Column(VARCHAR(LABEL_LENGTH))
+
 
 # User Tables
 
-USER = Table(
-    'User', meta,
-    Column('id', INT, primary_key=True),
-    Column('email', VARCHAR(EMAIL_LENGTH), unique=True),
-    Column('pwdHash', CHAR(PWD_HASH_LENGTH)),
-    Column('confirmed', BOOLEAN, default=False),
-)
+
+class User(Base):
+    __tablename__ = "User"
+
+    id = Column(INT, primary_key=True)
+    email = Column(VARCHAR(EMAIL_LENGTH), unique=True)
+    pwdHash = Column(CHAR(PWD_HASH_LENGTH))
+    confirmed = Column(BOOLEAN, default=False)
+
 
 # Quiz Tables
 
-QUIZ = Table(
-    'Quiz', meta,
-    Column('id', CHAR(UUID_LENGTH), primary_key=True),
-    Column('questionCount', SMALLINT),
-    Column('currentQuestion', SMALLINT),
-    Column('title', NVARCHAR(TITLE_LENGTH)),
-    Column('timeStart', INT),
-    Column('userId', INT, ForeignKey('User.id')),
-    Column('beltMin', SMALLINT, ForeignKey('Belt.id'), default=1),
-    Column('beltMax', SMALLINT, ForeignKey('Belt.id'), default=5)
-)
+class Quiz(Base):
+    __tablename__ = "Quiz"
 
-QUESTION = Table(
-    'Question', meta,
-    Column('id', INT, primary_key=True),
-    Column('text', NVARCHAR(TEXT_LENGTH)),
-    Column('optionCount', SMALLINT),
-    Column('quizId', CHAR(UUID_LENGTH), ForeignKey('Quiz.id')),
-    Column('infoId', INT, ForeignKey('Info.id', ondelete="CASCADE")),
-    Column('answerId', INT, ForeignKey('Answer.id', ondelete="CASCADE"))
-)
+    id = Column(CHAR(UUID_LENGTH), primary_key=True),
+    questionCount = Column(SMALLINT),
+    currentQuestion = Column(SMALLINT),
+    title = Column(NVARCHAR(TITLE_LENGTH)),
+    timeStart = Column(INT),
+    userId = Column(INT, ForeignKey('User.id')),
+    beltMin = Column(SMALLINT, ForeignKey('Belt.id'), default=1),
+    beltMax = Column(SMALLINT, ForeignKey('Belt.id'), default=5)
 
-OPTION = Table(
-    'Option', meta,
-    Column('id', INT, primary_key=True),
-    Column('text', NVARCHAR(TEXT_LENGTH)),
-    Column('questionId', INT, ForeignKey('Question.id', ondelete="CASCADE"))
-)
 
-ANSWER = Table(
-    'Answer', meta,
-    Column('id', INT, primary_key=True),
-    Column('quizId', INT, ForeignKey('Quiz.id', ondelete="CASCADE")),
-    Column('infoId', INT, ForeignKey('Info.id')),
-    Column('correct', BOOLEAN)
-)
+class Question(Base):
+    __tablename__ = "Question"
 
-RESULT = Table(
-    'Result', meta,
-    Column('id', INT, primary_key=True),
-    Column('userId', INT, ForeignKey('User.id')),
-    Column('correctCount', SMALLINT),
-    Column('incorrectCount', SMALLINT),
-    Column('timeSpent', INT),
-    Column('beltMin', SMALLINT, ForeignKey('Belt.id'), default=1),
-    Column('beltMax', SMALLINT, ForeignKey('Belt.id'), default=5)
-)
+    id = Column(INT, primary_key=True),
+    text = Column(NVARCHAR(TEXT_LENGTH)),
+    optionCount = Column(SMALLINT),
+    quizId = Column(CHAR(UUID_LENGTH), ForeignKey('Quiz.id')),
+    infoId = Column(INT, ForeignKey('Info.id', ondelete="CASCADE")),
+    answerId = Column(INT, ForeignKey('Answer.id', ondelete="CASCADE"))
+
+
+# OPTION = Table(
+#     'Option', meta,
+#     Column('id', INT, primary_key=True),
+#     Column('text', NVARCHAR(TEXT_LENGTH)),
+#     Column('questionId', INT, ForeignKey('Question.id', ondelete="CASCADE"))
+# )
+#
+# ANSWER = Table(
+#     'Answer', meta,
+#     Column('id', INT, primary_key=True),
+#     Column('quizId', INT, ForeignKey('Quiz.id', ondelete="CASCADE")),
+#     Column('infoId', INT, ForeignKey('Info.id')),
+#     Column('correct', BOOLEAN)
+# )
+#
+# RESULT = Table(
+#     'Result', meta,
+#     Column('id', INT, primary_key=True),
+#     Column('userId', INT, ForeignKey('User.id')),
+#     Column('correctCount', SMALLINT),
+#     Column('incorrectCount', SMALLINT),
+#     Column('timeSpent', INT),
+#     Column('beltMin', SMALLINT, ForeignKey('Belt.id'), default=1),
+#     Column('beltMax', SMALLINT, ForeignKey('Belt.id'), default=5)
+# )
 
 # Junction Tables
 
-QUIZ_CATEGORY = Table(
-    'QuizCategory', meta,
-    Column('id', INT, primary_key=True),
-    Column('quizId', CHAR(UUID_LENGTH), ForeignKey('Quiz.id', ondelete="CASCADE")),
-    Column('categoryId', INT, ForeignKey('Category.id', ondelete="CASCADE"))
-)
-
-RESULT_CATEGORY = Table(
-    'ResultCategory', meta,
-    Column('id', INT, primary_key=True),
-    Column('resultId', INT, ForeignKey('Result.id', ondelete="CASCADE")),
-    Column('categoryId', INT, ForeignKey('Category.id', ondelete="CASCADE"))
-)
+# QUIZ_CATEGORY = Table(
+#     'QuizCategory', meta,
+#     Column('id', INT, primary_key=True),
+#     Column('quizId', CHAR(UUID_LENGTH), ForeignKey('Quiz.id', ondelete="CASCADE")),
+#     Column('categoryId', INT, ForeignKey('Category.id', ondelete="CASCADE"))
+# )
+#
+# RESULT_CATEGORY = Table(
+#     'ResultCategory', meta,
+#     Column('id', INT, primary_key=True),
+#     Column('resultId', INT, ForeignKey('Result.id', ondelete="CASCADE")),
+#     Column('categoryId', INT, ForeignKey('Category.id', ondelete="CASCADE"))
+# )
 
 # Database Init
 
 if __name__ == '__main__':
-    meta.create_all(engine)
+    engine = create_engine(DB_CONNECTION_STRING, echo=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # insert_user = USER.insert()
+    # insert_user.execute(email="soren.seeberg@gmail.com", pwdHash="abcd"*16)
